2aa5b22ca61cd05bc69d65de163227370ae4a371
Ronnakorn Rattanakornphan <38160239+ronnakorn9@users.noreply.github.com>
2023-09-18 18:14:55 -0400

    visit and edit the rest of lab 1

--- lab1/lab1_filter.R
+++ lab1/lab1_filter.R
@@ -1,7 +1,8 @@
 #Landlock
 EPILand<-EPI[!Landlock]
-Eland <- EPILand[!is.na(EPILand)]
+ELand <- EPILand[!is.na(EPILand)]
 #
 hist(ELand)
+# capped x-axis within 30 - 95, set bin size to 1 and set y-axis to probability distribution
 hist(ELand, seq(30., 95., 1.0), prob=TRUE)
 
--- lab1/lab1_summary.R
+++ lab1/lab1_summary.R
@@ -2,11 +2,14 @@ summary(EPI) 	# stats
 fivenum(EPI,na.rm=TRUE)
 help(stem)
 stem(EPI)		 # stem and leaf plot
+# plot histrogram
 help(hist)
 hist(EPI)
 hist(EPI, seq(30., 95., 1.0), prob=TRUE)
+# add line plot
 help(lines)
 lines(density(EPI,na.rm=TRUE,bw=1.)) # or try bw=“SJ”
+# add "rug" the frequency bar on x-axis
 help(rug)
 rug(EPI) 
 
