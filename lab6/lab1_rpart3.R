aa22303c15ef51d1fac73c4aceb7960a777303cd
Ronnakorn Rattanakornphan <38160239+ronnakorn9@users.noreply.github.com>
2023-11-07 10:53:47 -0500

    finish the rpart

--- lab6/lab1_rpart3.R
+++ lab6/lab1_rpart3.R
@@ -3,8 +3,11 @@ library(rpart)
 data(Glass, package="mlbench")
 index <- 1:nrow(Glass)
 testindex <- sample(index, trunc(length(index)/3))
+
+### train-set split 2:1
 testset <- Glass[testindex,]
 trainset <- Glass[-testindex,]
+
 rpart.model <- rpart(Type ~ ., data = trainset)
 rpart.pred <- predict(rpart.model, testset[,-10], type = "class")
 printcp(rpart.model)
